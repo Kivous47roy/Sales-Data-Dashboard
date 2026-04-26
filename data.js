d1fe5d4ca9a89966e4a66bd9417252293f3a6f7c
Kivous47roy <128313847+Kivous47roy@users.noreply.github.com>
2026-04-26 16:45:07 +0530

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-import { getDashboardData } from "./_data.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
-  const data = await getDashboardData();
-  res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate=600");
-  return res.status(200).json(data);
-}
